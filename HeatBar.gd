ab9ded91a335d5aad247ee2a472bec3fd7aae27f
jcxldxr816 <james@the-calders.com>
2026-04-20 16:47:35 -0500

    final changes

--- HeatBar.gd
+++ HeatBar.gd
@@ -20,6 +20,7 @@ var _bar: ColorRect
 var _background: ColorRect
 var _label: Label
 var _caught_flash: ColorRect
+var _tag_count_label: Label
 
 # ---------------------------------------------------------------------------
 # Config
@@ -38,6 +39,9 @@ func _ready() -> void:
 	HeatManager.heat_changed.connect(_on_heat_changed)
 	HeatManager.player_caught.connect(_on_player_caught)
 	HeatManager.heat_reset.connect(_on_heat_reset)
+	HeatManager.heat_reset.connect(func(): _update_tag_count())
+	TaggingManager.tag_placed.connect(_on_tag_placed)
+	_update_tag_count()
 
 # ---------------------------------------------------------------------------
 # Signal handlers
@@ -60,6 +64,14 @@ func _on_player_caught() -> void:
 func _on_heat_reset() -> void:
 	_caught_flash.hide()
 	_caught_flash.modulate.a = 0.8
+	
+func _on_tag_placed(design: TagDesign) -> void:
+	_update_tag_count()
+
+func _update_tag_count() -> void:
+	var placed := TaggingManager.get_all_placed_designs().size()
+	var total := get_tree().get_nodes_in_group("tag_spots").size()
+	_tag_count_label.text = "TAGS  %d / %d" % [placed, total]
 
 # ---------------------------------------------------------------------------
 # UI construction
@@ -71,7 +83,7 @@ func _build_ui() -> void:
 	offset_left = -(BAR_WIDTH + MARGIN * 2 + 8)
 	offset_top = MARGIN
 	offset_right = -MARGIN
-	offset_bottom = MARGIN + BAR_HEIGHT + 20
+	offset_bottom = MARGIN + BAR_HEIGHT + 44
 
 	# Background track.
 	_background = ColorRect.new()
@@ -106,3 +118,12 @@ func _build_ui() -> void:
 	_caught_flash.size = Vector2(4000, 4000)
 	_caught_flash.hide()
 	add_child(_caught_flash)
+	
+	_tag_count_label = Label.new()
+	_tag_count_label.text = "TAGS  0"
+	_tag_count_label.add_theme_font_size_override("font_size", 13)
+	_tag_count_label.size = Vector2(BAR_WIDTH, 18)
+	_tag_count_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+	# Position it just below the heat bar
+	_tag_count_label.position = Vector2(0, BAR_HEIGHT + 24)
+	add_child(_tag_count_label)
